7685c684efdbc2a432cdd4f66f9a15f9304afff2
oiandin <putriroroandini@gmail.com>
2024-05-21 21:38:39 +0700

    modified:   src/components/Registration.vue

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -1,8 +1,12 @@
-<script setup lang="ts">
-  import {Button} from 'src/components/ui/button/*';
-  import {Card} from 'src/components/ui/card/*';
-  import {Input} from 'src/components/ui/input/*';
-  import {Label} from 'src/components/ui/label/label.vue';
+<script>
+  import { Button } from '@/components/ui/button'
+  import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+  import { Input } from '@/components/ui/input'
+  import { Label } from '@/components/ui/label'
+
+    export default {
+      name: 'Registration'
+    }
 </script>
 
 <template>
