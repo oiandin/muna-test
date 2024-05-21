ef8cac1a2691aa44e8c219b7d230700bd8e9533f
oiandin <putriroroandini@gmail.com>
2024-05-21 21:20:27 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+  import Nav from './components/Navbar.vue';
+  import Login from './components/Login.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      Nav,
+      Login
+    }
+  };
+</script>
+
+<template>
+  <div id="app">
+    <Nav />
+  </div>
+  <div>
+    <Login />
+  </div>
+</template>
---
+++ src/components/Registration.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+  import {Button} from 'src/components/ui/button/*';
+  import {Card} from 'src/components/ui/card/*';
+  import {Input} from 'src/components/ui/input/*';
+  import {Label} from 'src/components/ui/label/label.vue';
+</script>
+
+<template>
+  <Card class="mx-auto max-w-sm">
+    <CardHeader>
+      <CardTitle class="text-xl"> Sign Up </CardTitle>
+      <CardDescription>
+        Enter your information to create an account
+      </CardDescription>
+    </CardHeader>
+    <CardContent>
+      <div class="grid gap-4">
+        <div class="grid grid-cols-2 gap-4">
+          <div class="grid gap-2">
+            <Label for="first-name">First name</Label>
+            <Input id="first-name" placeholder="Max" required />
+          </div>
+          <div class="grid gap-2">
+            <Label for="last-name">Last name</Label>
+            <Input id="last-name" placeholder="Robinson" required />
+          </div>
+        </div>
+        <div class="grid gap-2">
+          <Label for="email">Email</Label>
+          <Input id="email" type="email" placeholder="m@example.com" required />
+        </div>
+        <div class="grid gap-2">
+          <Label for="password">Password</Label>
+          <Input id="password" type="password" />
+        </div>
+        <Button type="submit" class="w-full"> Create an account </Button>
+        <Button variant="outline" class="w-full"> Sign up with GitHub </Button>
+      </div>
+      <div class="mt-4 text-center text-sm">
+        Already have an account?
+        <a href="#" class="underline"> Sign in </a>
+      </div>
+    </CardContent>
+  </Card>
+</template>
